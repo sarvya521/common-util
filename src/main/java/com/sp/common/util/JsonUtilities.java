830a6ee8b3996f24f0d0c12dc6c64f376028e6c4
Sarvesh Padwal <sarveshp@unifytech.com>
2019-11-14 21:02:07 +0530

    Updated jackson libraries to fix security vulnerabilities

--- src/main/java/com/sp/common/util/JsonUtilities.java
+++ src/main/java/com/sp/common/util/JsonUtilities.java
@@ -1,17 +1,17 @@
 package com.sp.common.util;
 
-import java.io.IOException;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+import java.io.IOException;
 
 public final class JsonUtilities {
 
     private JsonUtilities() {
         throw new AssertionError(); // extra defensive
     }
-    
+
     public static String convertJsonToYaml(String jsonString) throws IOException {
         JsonNode jsonNodeTree = new ObjectMapper().readTree(jsonString);
         return new YAMLMapper().writeValueAsString(jsonNodeTree);
